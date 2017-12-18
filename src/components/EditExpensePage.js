5845a008049bab460e26a36e06b9ce3bd48aa53b
Simone Taeggi <simone.taeggi@leovegas.com>
2017-12-18 17:39:31 +0100

    Query Strings and URL Parameters

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-  <div>
-    This is from my edit Expense Component
-  </div>
-);
+const EditExpensePage = (props) => {
+  return (
+    <div>
+      Editing the expense with id of {props.match.params.id}
+    </div>
+  );
+};
 
 export default EditExpensePage;
