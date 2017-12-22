462ae285ed0392c90f3797222bc918f2b818e323
Simone Taeggi <simone.taeggi@leovegas.com>
2017-12-22 16:08:06 +0100

    Working with multiple Reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,54 @@
 import { createStore, combineReducers } from 'redux';
 
+// ADD_EXPENSE
+// REMOVE_EXPENSE
+// EDIT_EXPENSE
+// SET_TEXT_FILTER
+// SORT_BY_DATE
+// SORT_BY_AMOUNT
+// SET_START_DATE
+// SET_END_DATE
+
+const expensesReducerDefaultState = [];
+
+// Reducer per Expenses
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  // creo lo switch per le varie Actions
+  switch (action.type) {
+    // caso default ritorna lo state invariato
+    default:
+      return state;
+  }
+};
+
+
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  startDate: undefined,
+  endDate: undefined
+};
+
+// Reducer per Filters
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  // creo lo switch per le varie Actions
+  switch (action.type) {
+    // caso default
+    default:
+      return state;
+  }
+}
+
+// creazione dello STORE
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer
+  })
+);
+
+console.log(store.getState());
+
 // oggetto di cui vogliamo tenere traccia
 const demoState = {
   expenses: [{
