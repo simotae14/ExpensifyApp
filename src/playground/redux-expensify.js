c3014f2be5d10ae7812899606695a7ae45973ae3
Simone Taeggi <simone.taeggi@leovegas.com>
2018-01-02 12:14:15 +0100

    Implementati i Filtri sulle Spese

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,17 +1,15 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers } from "redux";
 
-import uuid from 'uuid';
+import uuid from "uuid";
 
 // AGGIUNGI_SPESA
-const aggiungiSpesa = (
-    { 
-      descrizione = '', 
-      note = '', 
-      importo = 0, 
-      creataAlle = 0 
-    } = {}
-  ) => ({
-  type: 'AGGIUNGI_SPESA',
+const aggiungiSpesa = ({
+  descrizione = "",
+  note = "",
+  importo = 0,
+  creataAlle = 0
+} = {}) => ({
+  type: "AGGIUNGI_SPESA",
   spesa: {
     id: uuid(),
     descrizione,
@@ -23,42 +21,42 @@ const aggiungiSpesa = (
 
 // RIMUOVI_SPESA
 const rimuoviSpesa = ({ id } = {}) => ({
-  type: 'RIMUOVI_SPESA',
+  type: "RIMUOVI_SPESA",
   id
 });
 
 // MODIFICA_SPESA
 const modificaSpesa = (id, aggiornamenti) => ({
-  type: 'MODIFICA_SPESA',
+  type: "MODIFICA_SPESA",
   id,
   aggiornamenti
 });
 
 // SET_TESTO_FILTRO
-const setTestoFiltro = (testo = '') => ({
-  type: 'SET_TESTO_FILTRO',
+const setTestoFiltro = (testo = "") => ({
+  type: "SET_TESTO_FILTRO",
   testo
 });
 
 // ORDINATO_PER_DATA
 const ordinatoPerData = () => ({
-  type: 'ORDINATO_PER_DATA'
+  type: "ORDINATO_PER_DATA"
 });
 
 // ORDINATO_PER_IMPORTO
 const ordinatoPerImporto = () => ({
-  type: 'ORDINATO_PER_IMPORTO'
+  type: "ORDINATO_PER_IMPORTO"
 });
 
 // SET_DATA_INIZIO
-const setDataInizio = (dataInizio) => ({
-  type: 'SET_DATA_INIZIO',
+const setDataInizio = dataInizio => ({
+  type: "SET_DATA_INIZIO",
   dataInizio
 });
 
 // SET_END_DATE
-const setDataFine = (dataFine) => ({
-  type: 'SET_DATA_FINE',
+const setDataFine = dataFine => ({
+  type: "SET_DATA_FINE",
   dataFine
 });
 
@@ -69,17 +67,14 @@ const speseReducer = (state = speseReducerDefaultState, action) => {
   // creo lo switch per le varie Actions
   switch (action.type) {
     // caso di aggiunta spesa
-    case 'AGGIUNGI_SPESA':
-      return [ 
-        ...state, 
-        action.spesa
-      ];
+    case "AGGIUNGI_SPESA":
+      return [...state, action.spesa];
     // caso di rimozione spesa
-    case 'RIMUOVI_SPESA':
+    case "RIMUOVI_SPESA":
       return state.filter(({ id }) => id !== action.id);
     // caso default ritorna lo state invariato
-    case 'MODIFICA_SPESA':
-      return state.map((spesa) => {
+    case "MODIFICA_SPESA":
+      return state.map(spesa => {
         if (spesa.id === action.id) {
           return {
             ...spesa,
@@ -88,16 +83,15 @@ const speseReducer = (state = speseReducerDefaultState, action) => {
         } else {
           return spesa;
         }
-      })
+      });
     default:
       return state;
   }
 };
 
-
 const filtriReducerDefaultState = {
-  testo: '',
-  ordinatoPer: 'data',
+  testo: "",
+  ordinatoPer: "data",
   dataInizio: undefined,
   dataFine: undefined
 };
@@ -107,31 +101,31 @@ const filtriReducer = (state = filtriReducerDefaultState, action) => {
   // creo lo switch per le varie Actions
   switch (action.type) {
     // caso modifica testo filtro
-    case 'SET_TESTO_FILTRO':
+    case "SET_TESTO_FILTRO":
       return {
         ...state,
         testo: action.testo
       };
     // caso ordinato per importo
-    case 'ORDINATO_PER_IMPORTO':
+    case "ORDINATO_PER_IMPORTO":
       return {
         ...state,
-        ordinatoPer: 'importo'
+        ordinatoPer: "importo"
       };
     // caso ordinato per data
-    case 'ORDINATO_PER_DATA':
+    case "ORDINATO_PER_DATA":
       return {
         ...state,
-        ordinatoPer: 'data'
+        ordinatoPer: "data"
       };
     // caso imposta data inizio
-    case 'SET_DATA_INIZIO':
+    case "SET_DATA_INIZIO":
       return {
         ...state,
         dataInizio: action.dataInizio
       };
     // caso imposta data fine
-    case 'SET_DATA_FINE':
+    case "SET_DATA_FINE":
       return {
         ...state,
         dataInizio: action.dataFine
@@ -140,7 +134,32 @@ const filtriReducer = (state = filtriReducerDefaultState, action) => {
     default:
       return state;
   }
-}
+};
+
+// CREO IL METODO CHE FILTRA LE MIE SPESE
+// Recupero le spese visibili
+const getSpeseVisibili = (
+  spese,
+  { testo, ordinatoPer, dataInizio, dataFine }
+) => {
+  // uso il filter sulle spese per filtrare in base ai parametri settati
+  return spese.filter(spesa => {
+    // istanzio i booleani che si occupano dei vari filtri per attivarli
+    // il filter seleziona solo gli elementi che soddisfano condizione, ovvero che se il filtro ha dataInizio le spese
+    // devono avere pty creataAlle >= a quel filtro
+    const dataInizioMatch =
+      typeof dataInizio !== "number" || spesa.creataAlle >= dataInizio;
+    // il filter seleziona solo gli elementi che soddisfano condizione, ovvero che se il filtro ha dataInizio le spese
+    // devono avere pty creataAlle >= a quel filtro
+    const dataFineMatch =
+      typeof dataFine !== "number" || spesa.creataAlle <= dataFine;
+    // il filter seleziona solo gli elementi che soddisano la condizione, ovvero in sto caso se il testo passato è incluso
+    // nella descrizione della spesa
+    const testoMatch = spesa.descrizione.toLowerCase().includes(testo.toLowerCase());
+
+    return dataInizioMatch && dataFineMatch && testoMatch;
+  });
+};
 
 // creazione dello STORE
 const store = createStore(
@@ -151,20 +170,27 @@ const store = createStore(
 );
 
 store.subscribe(() => {
-  console.log(store.getState());
+  const state = store.getState();
+  // recupero le spese visibili
+  const speseVisibili = getSpeseVisibili(state.spese, state.filtri);
+  console.log(speseVisibili);
 });
 
 // invoco il metodo addExpense
-// const spesaUno = store.dispatch(aggiungiSpesa({ descrizione: 'Affitto', importo: 100}));
-// const spesaDue = store.dispatch(aggiungiSpesa({ descrizione: 'Caffé', importo: 300}));
+const spesaUno = store.dispatch(
+  aggiungiSpesa({ descrizione: "Affitto", importo: 100, creataAlle: 1000 })
+);
+const spesaDue = store.dispatch(
+  aggiungiSpesa({ descrizione: "Caffé", importo: 300, creataAlle: -1000 })
+);
 
 // store.dispatch(rimuoviSpesa({ id: spesaUno.spesa.id }));
 
 // // modifico la seconda spesa
 // store.dispatch(modificaSpesa(spesaDue.spesa.id, { importo: 500 }));
 
-// // modifico il testo del filtro di ricerca
-// store.dispatch(setTestoFiltro('affitto'));
+// modifico il testo del filtro di ricerca
+store.dispatch(setTestoFiltro('ff'));
 // // modifico il testo del filtro di ricerca
 // store.dispatch(setTestoFiltro());
 
@@ -173,28 +199,29 @@ store.subscribe(() => {
 // // modifico ordinamento per data
 // store.dispatch(ordinatoPerData());
 
-// setto la data di Inizio
-store.dispatch(setDataInizio(125));
-store.dispatch(setDataInizio());
+// // setto la data di Inizio
+// store.dispatch(setDataInizio(0));
+// store.dispatch(setDataInizio());
 
-// setto la data di Fine
-store.dispatch(setDataFine(1250));
-
+// // setto la data di Fine
+// store.dispatch(setDataFine(1250));
 
 // oggetto di cui vogliamo tenere traccia
 const demoState = {
-  spese: [{
-    id: 'jakljkljkaldjk',
-    descrizione: 'January Rent',
-    note: 'This was the final payment for that address',
-    importo: 54500,
-    creataAlle: 0
-  }],
+  spese: [
+    {
+      id: "jakljkljkaldjk",
+      descrizione: "January Rent",
+      note: "This was the final payment for that address",
+      importo: 54500,
+      creataAlle: 0
+    }
+  ],
   // filters cui do un nome e un valore
   // posso settare anche un range
   filtri: {
-    testo: 'affitto',
-    ordinatoPer: 'importo', // per data o importo
+    testo: "affitto",
+    ordinatoPer: "importo", // per data o importo
     dataInizio: undefined,
     dataFine: undefined
   }
